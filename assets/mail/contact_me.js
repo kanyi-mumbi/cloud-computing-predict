5126290453555152b1af5e6050894a6192af4259
kanyi-mumbi <99389155+kanyi-mumbi@users.noreply.github.com>
2022-05-09 15:05:07 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://imi37durmj.execute-api.eu-west-1.amazonaws.com/default/function",
+                url: "https://bid73v2rab.execute-api.eu-west-1.amazonaws.com/default/portfolio",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
